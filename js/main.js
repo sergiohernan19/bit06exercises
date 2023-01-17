cf29e692b702c6024cd0a54ff02194793a28899f
Sergio Hernández <sehb1234@gmail.com>
2023-01-17 00:58:46 -0500

    Login creado

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function login(){
+var user, password
+
+user = document.getElementById("usuario").value;
+password = documente.getElementById("contrasena").value;
+
+if( user == "bit" && password == "2023"){
+alert("¡Iniciaste Sesión!")
+}
+ else{
+    alert("Datos Incorrectos")  
+}
+}
+
+    
